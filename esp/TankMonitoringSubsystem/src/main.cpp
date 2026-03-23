874eedd2e3068a732a525e25cbd526c84907d541
Giuseppe Cattolico <giuseppe.cattolico@studio.unibo.it>
2026-03-23 17:55:48 +0100

    Configure PlatformIO credentials and implement MQTT client

--- esp/TankMonitoringSubsystem/src/main.cpp
+++ esp/TankMonitoringSubsystem/src/main.cpp
@@ -1,18 +1,107 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#define MSG_BUFFER_SIZE 50
 
-// put function declarations here:
-int myFunction(int, int);
+/* wifi network info */
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+/* MQTT server address */
+const char *mqtt_server = "broker.mqtt-dashboard.com";
+
+/* MQTT topic */
+const char *topic = "esiot-2025";
+
+/* MQTT client management */
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+unsigned long lastMsgTime = 0;
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi()
+{
+  delay(10);
+  Serial.println(String("Connecting to ") + WIFI_SSID);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/* MQTT subscribing callback */
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.println(String("Message arrived on [") + topic + "] len: " + length + " txt: " + String((char *)payload, length));
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void reconnect()
+{
+
+  // Loop until we're reconnected
+
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+
+    // Create a random client ID
+    String clientId = String("esiot-2025-client-") + String(random(0xffff), HEX);
+
+    // Attempt to connect
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+      client.subscribe(topic);
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  setup_wifi();
+  randomSeed(micros());
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop()
+{
+
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsgTime > 10000)
+  {
+    lastMsgTime = now;
+    value++;
+
+    /* creating a msg in the buffer */
+    snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+
+    Serial.println(String("Publishing message: ") + msg);
+
+    /* publishing the msg */
+    client.publish(topic, msg);
+  }
 }
